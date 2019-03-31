b8c02f7213376ce38e815a92d32afa9541c48487
shayne651 <shayne651@gmail.com>
2019-03-31 00:00:24 -0400

    fixed adding textvalues

--- mongoText.js
+++ mongoText.js
@@ -5,8 +5,16 @@ var url = "mongodb://localhost:27017/text";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dataobject = db.db("textContent");
-  var insert = {username:"shayne",content:'DHDHDHDHDHDH',bold:0,italics:0,fontsize:12}
-  dataobject.collection("textContent").insertOne(insert,function(err,res){
+  var insert = new Array(7)
+  insert[0] = {username:"acc1",content:'account - acc1',bold:1,italics:0,fontsize:12}
+  insert[1] = {username:"acc2",content:'account - acc2',bold:0,italics:1,fontsize:19}
+  insert[2] = {username:"account",content:'account - account',bold:1,italics:0,fontsize:22}
+  insert[3] = {username:"profile1",content:'account - profile1',bold:0,italics:0,fontsize:32}
+  insert[4] = {username:"fake-login",content:'account - fake-login',bold:1,italics:1,fontsize:42}
+  insert[5] = {username:"noanaccount",content:'account - notanaccount',bold:1,italics:0,fontsize:52}
+  insert[6] = {username:"shayne",content:'account - shayne',bold:0,italics:1,fontsize:62}
+  
+  dataobject.collection("textContent").insertMany(insert,function(err,res){
   	if(err) throw err;
   })
   console.log("contentAdded");
